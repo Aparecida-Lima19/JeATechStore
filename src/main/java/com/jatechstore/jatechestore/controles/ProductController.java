8eebe985c55abd504440c4111b9de5ce2f66655e
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2025-05-23 10:18:12 -0300

    Inseri o metodo POST

--- src/main/java/com/jatechstore/jatechestore/controles/ProductController.java
+++ src/main/java/com/jatechstore/jatechestore/controles/ProductController.java
@@ -1,14 +1,12 @@
 package com.jatechstore.jatechestore.controles;
 
 import com.jatechstore.jatechestore.dto.ProductDTO;
+import com.jatechstore.jatechestore.entities.Product;
 import com.jatechstore.jatechestore.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,12 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        dto = service.insert(dto);
+        return dto;
     }
 
 }
